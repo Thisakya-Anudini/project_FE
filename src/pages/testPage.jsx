1580a549b4a22aa74d1e7bad47871a6f47badf03
Thisakya-Anudini <thisakyaanudini@gmail.com>
2025-08-28 03:21:08 +0530

    Day 14,15🚀

--- src/pages/testPage.jsx
+++ src/pages/testPage.jsx
@@ -1,6 +1,46 @@
-import React from 'react'
+
+import { useState } from "react";
+
+import toast from "react-hot-toast";
+import mediaUpload from "../utils/mediaUpload";
+
+
+
+
 
 
 export default function TestPage() {
+
+    const [file, setFile] = useState(null);
+    function handleUpload(){
+        mediaUpload(file)
+        .then((url) => {
+            console.log(url)
+
+            
+        }).catch((error) => {
+            toast.error(error)
+        })
+    }
+     
+
+
+
+    return (
+        <div className="w-full h-screen flex justify-center items-center">
+            <input type="file" 
+           // accept="image/*" 
+            onChange={
+                (e) => {
+                                    
+                 setFile(e.target.files[0])   
+                    
+                    
+                    
+                }
+            }/>
+            <button onClick={handleUpload} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md cursor-pointer">Upload</button>
+        </div>
+    );
 
 }
--- src/utils/mediaUpload.jsx
+++ src/utils/mediaUpload.jsx
@@ -1,41 +1,45 @@
-const url = "https://xboibwswujlzqjcxjdtp.supabase.co"
-const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhib2lid3N3dWpsenFqY3hqZHRwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU5NDE4MTgsImV4cCI6MjA3MTUxNzgxOH0.7OMeqHqxqK0RouD6uXQqlyjcPuMz74xmb-PeHh3L5S8"
-
+import { toast } from "react-hot-toast";
 import { createClient } from "@supabase/supabase-js";
 
-const supabase = createClient(url,key)
 
-export default function uploadFile(file){
-    const promise  = new Promise(
+const url = "https://uborvfzxoppbimjiqftj.supabase.co";
+const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVib3J2Znp4b3BwYmltamlxZnRqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYxODIyNTMsImV4cCI6MjA3MTc1ODI1M30.M9zP7ohd_K7EYZi2zhk0kAO6_ZoVvB_4r654ClmV7yY";
 
-        (resolve, reject)=>{
+// Initialize Supabase Client
+const supabase = createClient(url, key);
 
-            if(file == null){
-                reject("Please select a file to upload");
-                return;
-            }
-
-            const timeStamp = new Date().getTime();
-            const fileName = timeStamp+"-"+file.name
-
-            supabase.storage.from("images").upload(fileName,file,{
-                cacheControl: "3600",
-                upsert: false
-            }).then(
-                ()=>{
-                    const publicUrl = supabase.storage.from("images").getPublicUrl(fileName).data.publicUrl;
-                    resolve(publicUrl)
-                }
-            ).catch(
-                ()=>{
-                    reject("Failed to upload file");
-                }
-            )
-            
-
+export default function mediaUpload(file) {
+    const promise = new Promise((resolve, reject) => {
+        if (file == null) {
+     
+            toast.error("Please select a file to upload");
+            return;
         }
 
-    )
+        const timeStamp = new Date().getTime();
+        const fileName = file.name + "_" + timeStamp;
+
+        supabase.storage.from("images").upload(fileName, file, {
+            cacheControl: "3600",
+            upsert: false,
+        })
+        .then(() => {
+            const { publicUrl } = supabase
+                .storage
+                .from("images")
+                .getPublicUrl(fileName).data;
+
+            console.log("File uploaded successfully", publicUrl);
+            toast.success("File uploaded successfully");
+            resolve(publicUrl);
+        })
+        .catch(() => {
+            
+            reject("File upload failed");
+          
+         
+        });
+    });
+
     return promise;
 }
-
