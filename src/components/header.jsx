d5892fa8948edcb3735133d849d9362737f6aee4
Thisakya-Anudini <thisakyaanudini@gmail.com>
2025-09-02 18:55:11 +0530

    Day 18 🚀

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,13 +4,12 @@ import { Link } from "react-router-dom";
 export default function Header(){
     return(
 
-        <header className=" h-[100px] bg-blue-500 flex justify-center items-center"> 
+        <header className=" h-[100px] bg-blue-500 flex justify-center items-center gap-[30px]"> 
         <Link to ="/" className="text-white text-2xl font-bold">Home</Link>
         <Link to ="/products" className="text-white text-xl ml-5 font-bold">Products</Link>
         <Link to ="/reviews" className="text-white text-xl ml-5 font-bold">Reviews</Link>
-        <Link to ="/cart" className="text-white text-xl ml-5 font-bold">Cart</Link>
-        <Link to ="/orders" className="text-white text-xl ml-5 font-bold">Orders</Link>
         <Link to ="/about-us" className="text-white text-xl ml-5 font-bold">About Us</Link>
+        <Link to ="/contact-us" className="text-white text-xl ml-5 font-bold">Contact Us</Link>
 
             
         </header>
--- src/pages/client/productOverview.jsx
+++ src/pages/client/productOverview.jsx
@@ -1,16 +1,17 @@
 import { useEffect, useState } from "react";
-import {  useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import axios from "axios";
 import Loader from "../../components/loader";
 import ImageSlider from "../../components/imageSlider";
 
+
 export default function ProductOverviewPage() {
-    const params= useParams();
-    const [product, setProduct]=useState([]);
-    const[status, setStatus]= useState("loading");//loading state,success state,error state are the  3 states in this page
+    const params = useParams();
+    const [product, setProduct] = useState({});
+    const [status, setStatus] = useState("loading");
 
     useEffect(() => {
-        if (status==="loading") {
+        if (status === "loading") {
             axios
                 .get(import.meta.env.VITE_BACKEND_URL + "/api/products/" + params.productId)
                 .then((res) => {
@@ -24,31 +25,45 @@ export default function ProductOverviewPage() {
         }
     }, [status]);
 
-
-
-
     return (
-        <div className="w-full h-full ">
-           {
-                status=="loading"&&<Loader/>
-           }
-           {
-                status=="success"&& <div className="w-full h-full flex flex-row">
+        <div className="w-full h-full">
+            {status === "loading" && <Loader />}
 
-                    <div className="w-[49%] h-full  flex flex-col justify-center items-center">
-                        <ImageSlider images={product.images} /> 
-
+            {status === "success" && <div className="w-full h-full flex flex-row">
+                    <div className="w-[50%] h-full flex flex-col justify center items-center mt-15">
+                        <ImageSlider images={product.images} />
                     </div>
-                    
-                    
-                    <div className="w-[50%] h-full bg-red-500"></div>
 
+                    <div className="w-[50%] h-full flex flex-col items-center pt-[50px]">
+                        <h1 className="text-2xl font-bold">{product.name}<span className="text-gray-500 font-normal">{"  "}{product.altNames.join(" | ")} </span></h1>
+                        <p className="text-md mt-[20px]">{product.description}</p>
+                        <div className="w-full flex flex-col items-center mt-[20px]">
+                            {
+                                product.labelledPrice > product.price ?(
+                                    <div>
+                                        <span className=" text-xl line-through font-semibold mr-[20px]">{product.labelledPrice.toLocaleString('en-US',{minimumFractionDigits: 2, maximumFractionDigits: 2})} </span>
+                                        <span className="text-2xl font-bold ">{product.price.toLocaleString('en-US',{minimumFractionDigits: 2, maximumFractionDigits: 2})}</span>
+                                    </div>
+                                )
+                                :
+                                (
+                                    <div>
+                                        <span className="text-3xl font-bold">{product.price.toLocaleString('en-US',{minimumFractionDigits: 2, maximumFractionDigits: 2})}</span>
+                                    </div>
+                                )
+                            }
+
+                        </div>
+                        <div className="w-full flex flex-row justify-center items-center mt-[20px]  gap-[20px]">
+                            <button className="w-[200px] h-[50px] cursor-pointer rounded-xl shadow-2xl  text-white bg-blue-900 border-[3px] border-blue-900 hover:bg-white hover:text-blue-900">Buy Now</button>
+                            <button className="w-[200px] h-[50px] cursor-pointer rounded-xl shadow-2xl text-white  bg-blue-500 border-[3px] border-blue-500 hover:bg-white hover:text-blue-900 ">Add to Cart</button>
+
+                        </div>
+                    </div>
                 </div>
-           }
-           {
-                status=="error"&&<div>error</div>
-           }
-         
+            }
+
+            {status === "error" && <div>Error loading product data.</div>}
         </div>
     );
 }
